627f8df1b3df66d16b1cfd1a898ce1d9d30d7674
daniel <dejong543@gmail.com>
2020-06-23 00:58:15 +0200

    altered user model and ran migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        '''Create a new profile'''
+        if not email:
+            raise ValueError('User must have a valid email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def getFullName(self):
+        return self.name
+
+    def getShortName(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
